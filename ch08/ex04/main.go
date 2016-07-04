4bf67f218d510e3ba92cd58e48cea6d02212c777
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-07-05 02:55:11 +0900

    Add ex8.4

---
+++ ch08/ex04/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	var wg sync.WaitGroup
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go func(s string) {
+			echo(c, input.Text(), 1*time.Second)
+			wg.Done()
+		}(input.Text())
+	}
+
+	wg.Wait()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
